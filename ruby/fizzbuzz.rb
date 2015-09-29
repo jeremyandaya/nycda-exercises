67c621df65018b2f835f6d61e3b6becda4a3fae9
Jeremy Andaya <jeremy.andaya@gmail.com>
2015-09-29 14:13:09 -0400

    added fizzbuzz code

--- ruby/fizzbuzz.rb
+++ ruby/fizzbuzz.rb
@@ -1,1 +1,11 @@
-# REPLACE ME
+for i in (1..100) do
+  if i % 3 == 0 and i % 5 == 0
+     puts "FizzBuzz"
+  elsif i % 3 == 0
+     puts "Fizz"
+  elseif i % 5 == 0
+     puts "Buzz"
+  else
+     puts i
+  end
+end
