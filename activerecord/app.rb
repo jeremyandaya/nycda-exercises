e7a6f60a1f114841ee92ecedee42057a2071f646
Jeremy Andaya <jeremy.andaya@gmail.com>
2015-10-09 12:43:29 -0400

    updated app.rb and created index.erb

--- activerecord/app.rb
+++ activerecord/app.rb
@@ -1,4 +1,6 @@
 require 'active_record'
+require 'sinatra'
+require 'sinatra/activerecord'
 require 'pg'
 require './models'
 
@@ -11,3 +13,11 @@ ActiveRecord::Base.establish_connection(
 )
 
 c = ActiveRecord::Base.connection
+
+get "/" do
+	
+	params[:presidents] = President.all
+	# President.all.first.name
+	erb :index # this should be last
+	
+end
